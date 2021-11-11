94f9f1a2aa7ac70d6768525019c61ff40c5ba58c
shubhrajyotidey <toshubhrajyotidey@gmail.com>
2021-11-12 00:53:54 +0530

    Made changes in server.ts

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,17 @@ dotenv.config();
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
-app.use(cors())
+
+app.use((req, res, next) => {
+	console.log('Request Received', req.originalUrl, req.method);
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Origin,X-Requested-With,Content-Type,Accept,Authorization'
+	);
+	res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+	next();
+});
 
 /**-------- Paths ------------------- */
 
